ff235ed76bff39250365b817c8cdb60697aa3e27
kevinchicken <kevin@codingchickens.com>
2022-12-02 12:23:35 -0300

    fix: correct broke after sign up view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+  def after_sign_in_path_for(resource_or_scope)
+    landing_index_url
+  end
+
+  def after_sign_up_path_for(resource_or_scope)
+    after_sign_in_path_for(resource_or_scope)
+  end
 end
