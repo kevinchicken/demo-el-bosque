929e250e552acd2494429d0188f3db45b9fff107
kevinchicken <kevin@codingchickens.com>
2022-12-09 12:07:18 -0300

    feat: add role column to users table, confugure permissions to manage news

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  ROLES = %i[admin reader]
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_one_attached :image
+
+  def admin?
+    role == 'admin'
+  end
 
   def set_auth_token
     self.token = SecureRandom.base58(36)
